da9d2b47dc6eac33b0e215a389d78a657d63183d
Punyarit@Progtech <punyarit.kla.progtech@gmail.com>
2023-11-14 12:44:47 +0700

    update

--- promises/colorcode/app.js
+++ promises/colorcode/app.js
@@ -1,14 +1,40 @@
 const getWeather = () => {
   return new Promise((resolve, reject) => {
-    reject('Please check your internet connection');
-    resolve('Sunny');
+    setTimeout(() => {
+      resolve('Sunny');
+      reject('Check your internet connection');
+    }, 100)
   })
 }
 
+const getWeatherIcon = (weather) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      switch(weather){
+        case 'Sunny':
+          resolve('🌞');
+          break
+        case 'Cloudy':
+          resolve('☁️');
+          break
+        case 'Rainy':
+          resolve('☔');
+          break
+        default:
+          reject('No icon found');
+      }
+    }, 100)
+  })
+}
 const onSuccess = data => console.log(`😍 Success ${data}`);
 const onError = error => console.log(`🤢 Error ${error}`);
-const finallyFn = () => console.log(`🌹 Data from Punyarit Klaphachon 🌹`);
+const finallyFn = () => setTimeout(() => console.log(`🌹 Data from Punyarit Klaphachon 🌹`), 500);
 
 // 1st param = resolve = onSuccess
 // 2nd param = reject = onError
-getWeather().then(onSuccess, onError).finally(finallyFn());
+getWeather()
+  // getWeatherIcon(resolve of getWeather())
+  .then(getWeatherIcon) // the data of resolve go to the parameter of next function
+  .then(onSuccess, onError) // reject(error) of any promise will be display
+  .catch(onError)
+  .finally(finallyFn());
---
+++ promises/practice/app.js
@@ -0,0 +1,46 @@
+const getNumber = () => {
+  return new Promise((resolve, reject) => {
+    resolve(6);
+    reject('We\'re close');
+  })
+}
+
+const getRomanNumber = (number) => {
+  return new Promise((resolve, reject) => {
+    switch(number){
+      case 1:
+        resolve('I');
+        break;
+      case 2:
+        resolve('II');
+        break;
+      case 3:
+        resolve('III');
+        break;
+      case 4:
+        resolve('IV');
+        break;
+      case 5:
+        resolve('V');
+        break;
+      case 6:
+        resolve('VI');
+        break;
+      case 7:
+        resolve('VII');
+        break;
+      default:
+        reject('Number > 7 is not acceptable.');
+        break;
+    }
+  })
+}
+
+const onSuccess = (value) => console.log(`Success: ${value}`);
+const onError   = (error) => console.log(`Error: ${error}`);
+
+getNumber()
+  .then(getRomanNumber)
+  .then(onSuccess)
+  .catch(onError)
+  .finally(); // global error handler
