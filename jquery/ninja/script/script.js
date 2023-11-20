1bf73cbce6239c374c17b5a2eaa7d26b32502db4
Punyarit@Progtech <punyarit.kla.progtech@gmail.com>
2023-11-20 17:53:39 +0700

    update

--- jquery/ninja/script/script.js
+++ jquery/ninja/script/script.js
@@ -1,4 +1,29 @@
 // $ most likely are jQuery statement
+/* 
+  $('css-selector');
+  this will return jquery object with contain jquery method
+ */
+
+
 $(document).ready(function() {
-  alert('jquery loaded');
+  // alert('jquery loaded');
+  // console.log(document.getElementById('main-content'));
+  // console.log($('#main-content'));
+
+  // animation
+  const logoEl = $('#logo');
+
+  console.log('Upwrapped element:');
+  console.log(logoEl[0]);
+
+  logoEl.css({
+    display: 'inline',
+    position: 'relative'
+  })
+
+  logoEl.animate({
+    left: 190
+  })
+
+  document.getElementsByTagName('title').innerText = 'wowowow';
 });
